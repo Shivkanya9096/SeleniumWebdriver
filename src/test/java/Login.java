0c03ed0abe2481d85f30a41eec2a51648c186227
Shivkanya9096 <jadhav.shivkanya18@yahoo.com>
2021-05-09 15:23:21 +0530

    Added changes

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,49 +16,53 @@ import com.sun.jna.platform.win32.Netapi32Util.User;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
-	
-	 private static WebDriver driver;
-	 
-	 @BeforeTest
-	 public void Setup() {
-//		 String ProjectPath= System.getProperty("user.dir");
-//		 System.setProperty("webdriver.chrome.driver", ProjectPath+
-//                 "\\drivers\\chromedriver.exe");
-		 
-		 WebDriverManager.chromedriver().setup();
-		 driver = new ChromeDriver();
-		 driver.manage().window().maximize();
-		 driver.get("http://www.gmail.com/");
-	 }
-		 
+
+	private static WebDriver driver;
+
+	@BeforeTest
+	public void Setup() {
+		//		 String ProjectPath= System.getProperty("user.dir");
+		//		 System.setProperty("webdriver.chrome.driver", ProjectPath+
+		//                 "\\drivers\\chromedriver.exe");
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.gmail.com/");
+	}
+
 	@Test	 
-	 public void EnetrCred() {
-		 WebElement email= driver.findElement(By.xpath("//input[@type='email']"));
-		 new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(email));
-		 email.sendKeys("jadhav.shivkanya09@gmail.com");
-		 
-		 WebElement nextButton= driver.findElement(By.xpath("//div[@class='VfPpkd-RLmnJb']"));
-		 if(nextButton.isDisplayed()) {
-			 nextButton.click();
-		 }else {
-			 Assert.assertFalse(false,"Next button is not displayed");
-			 return;
-		 }
-		 
-		 WebElement passwrd= driver.findElement(By.xpath("//input[@type='password']"));
-		 new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(passwrd));
-		 passwrd.sendKeys("Shiv@12346");
-		 
-		 WebElement validationMsg= driver.findElement(By.xpath("//span[contains(text(),'Wrong password. Try again or click "
-		 		+ "Forgot password to reset it.')]"));
-		 
-		 boolean isPass = validationMsg.getText().contains("Wrong password.");
-		 Assert.assertEquals(true, isPass);
-	 }
-	 
+	public void EnetrCred() {
+		WebElement email= driver.findElement(By.xpath("//input[@type='email']"));
+		new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(email));
+		email.sendKeys("jadhav.shivkanya09@gmail.com");
+
+		WebElement nextButton= driver.findElement(By.xpath("//div[@class='VfPpkd-RLmnJb']"));
+		if(nextButton.isDisplayed()) {
+			nextButton.click();
+		}else {
+			Assert.assertFalse(false,"Next button is not displayed");
+			return;
+		}
+		WebElement passwrd= driver.findElement(By.xpath("//input[@type='password']"));
+		boolean isPass= false;
+		try {
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(passwrd));
+			passwrd.sendKeys("Shiv@12346");
+			WebElement validationMsg= driver.findElement(By.xpath("//span[contains(text(),'Wrong password. Try again or click "
+					+ "Forgot password to reset it.')]"));
+
+			 isPass = validationMsg.getText().contains("Wrong password.");
+		}catch (StaleElementReferenceException e){
+			System.out.println("Browser may not support...");
+		}
+		
+		Assert.assertEquals(true, isPass);
+	}
+
 	@AfterTest
-	 public void closebrowser() {
-		 driver.quit();
-	 }
+	public void closebrowser() {
+		driver.quit();
+	}
 
 }
